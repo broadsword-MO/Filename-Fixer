aa8d04beeb8aed2512296fd7d7aee31762b24b9c
broadsword-MO <62671314+broadsword-MO@users.noreply.github.com>
2023-02-22 14:13:07 -0600

    Changed direction from single purpose to multi- Added recent results, fill spaces, remove spaces, spinal-case, camelCase

--- HTML spaces replacer/main.js
+++ HTML spaces replacer/main.js
@@ -1,20 +1,76 @@
-// Line below doesn't work when this JS is in a file separate from the HTML
+// REVIEW - Lines below don't work when this JavaScript is in a file separate from the HTML. Why? Or is it normal?
+// const input = document.getElementById('input');
 // const output = document.getElementById('output');
-
-// Works good!
-function onInputChange() {
-    // .value only works to extract current text with 'input', 'form', 'textarea' etc.
-    const fileName = document.getElementById('input').value;
-    document.getElementById('output').innerHTML = fileName.replace(/\s/g, '%20');
-}
+// const recResults = document.getElementById('recentResults');
 
 const makeSelection = (elem) => window.getSelection().selectAllChildren(elem);
 const getSelectedText = () => window.getSelection().toString();
 const copyToClipboard = (text) =>
     navigator.clipboard?.writeText && navigator.clipboard.writeText(text);
+const recentResults = [];
+
+function addResultToRecentWithBreak() {
+    `${getSelectedText()}<br />` != recentResults[0] &&
+        recentResults.unshift(`${getSelectedText()}<br />`);
+    recentResults.length > 5 && recentResults.pop();
+}
 
 function selectStringCopy() {
     makeSelection(document.getElementById('output'));
-    getSelectedText();
+    addResultToRecentWithBreak();
     copyToClipboard(getSelectedText());
+    document.getElementById('recentResults').innerHTML = recentResults.join(' ');
+    document.getElementById('input').select();
 }
+
+// .value only works to extract current text with 'input', 'form', 'textarea' etc.
+function fillSpaces() {
+    const filename = document.getElementById('input').value;
+    const fixedFilename = filename
+        .replace(/^"|"$/g, '')
+        .trim()
+        .replace(/\s/g, '%20');
+    document.getElementById('output').innerHTML = fixedFilename;
+    selectStringCopy(fixedFilename);
+}
+
+function removeSpaces() {
+    const filename = document.getElementById('input').value;
+    const fixedFilename = filename
+        .replace(/^"|"$/g, '')
+        .trim()
+        .replace(/%20/g, ' ');
+    document.getElementById('output').innerHTML = fixedFilename;
+    selectStringCopy(fixedFilename);
+}
+
+function spinalCase() {
+    const filename = document.getElementById('input').value;
+    const fixedFilename = filename
+        .replace(/^"|"$/g, '')
+        .trim()
+        .replace(/(\w)[ _]?([A-Z])| |%20/g, '$1-$2')
+        .toLowerCase();
+    document.getElementById('output').innerHTML = fixedFilename;
+    selectStringCopy(fixedFilename);
+}
+
+function camelCase() {
+    const filename = document.getElementById('input').value;
+    const trimFilename = filename.replace(/^"|"$/g, '').trim();
+    let extension;
+    const splitName = trimFilename.split('.');
+    splitName.length > 1 && (extension = splitName.pop().toLowerCase());
+    const name = splitName
+        .join(' ')
+        .replace(/[-_.]|%20/g, ' ')
+        .replace(/\b\w/g, (char, index) =>
+            index === 0 ? char.toLowerCase() : char.toUpperCase()
+        )
+        .replace(/\s+/g, '');
+    const fixedFilename = extension ? `${name}.${extension}` : name;
+    document.getElementById('output').innerHTML = fixedFilename;
+    selectStringCopy(fixedFilename);
+}
+
+// This isYour%20other-file.html
